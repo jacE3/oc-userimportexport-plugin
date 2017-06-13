1818b865364029c1dde031654c12603e6fe4d596
jacE3 <jacE3@users.noreply.github.com>
2017-06-13 21:41:55 +0100

    Updated userexportsmodel to include country name

--- models/UserExportModel.php
+++ models/UserExportModel.php
@@ -7,11 +7,17 @@ class UserExportModel extends ExportModel
 {
     public function exportData($columns, $sessionKey = null)
     {
+        $return = [];
         $users = User::all();
-        $users->each(function($user) use ($columns) {
+        $users->each(function($user) use ($columns, &$return) {
             $user->addVisible($columns);
+            $array = $user->toArray();
+            if ($user->country && $user->country->name) {
+                $array['country_id'] = $user->country->name;
+            }
+            $return[] = $array;
         });
 
-        return $users->toArray();
+        return $return;
     }
 }
